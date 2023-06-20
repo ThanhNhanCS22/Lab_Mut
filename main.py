71171f92d3b31e663e049009da01bdd51715eadf
ConCopBinhDuong <136608139+ConCopBinhDuong@users.noreply.github.com>
2023-06-20 17:17:16 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+arr = []
+n = int(input("nhap pham vi cua mang : "))
+for i in range(n):
+
+    print("nhap vao phan tu o vi tri thu ", i, " : ", end="")
+    e = int(input())
+
+    arr.append(e)
+
+print(arr)
+
+def print_out(array):
+
+    print(array)
+
+    return
+
+print_out(arr)
+
+
+
+
+
