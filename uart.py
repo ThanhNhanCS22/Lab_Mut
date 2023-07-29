c988d6c57add98e7d061889e2c23c80886fc056a
ThanhNhanBD <136608139+ThanhNhanNewbie@users.noreply.github.com>
2023-07-29 12:19:15 +0700

    Add files via upload

---
+++ uart.py
@@ -0,0 +1,38 @@
+import serial.tools.list_ports
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    #return commPort
+    return "com..." #Ông xài com nao thi return com do
+if getPort() != "Nome" :
+
+    ser = serial.Serial(port=getPort(), baudrate=115200)
+def processData(client,data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[1] == "T":
+        client.publish("", splitData[2])
+
+mess = ""
+def readSerial(client):
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
