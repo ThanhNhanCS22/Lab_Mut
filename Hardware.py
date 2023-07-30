5882420d70b7a1539b14ebb9ec21c3dc12d9e3c7
ThanhNhanBD <136608139+ThanhNhanNewbie@users.noreply.github.com>
2023-07-30 11:21:48 +0700

    Add files via upload

---
+++ Hardware.py
@@ -0,0 +1,19 @@
+import random
+
+def humidity() :
+    value = random.randint( 50,80)
+    return value
+def temperature() :
+    value1 = random.randint(0,50)
+    return value1
+
+
+def write_to_file(values,filename):
+    with open('data.txt', 'w') as file:
+        for value in values :
+
+
+            file.write(str(value)+ "\n")
+
+def turn_on_AC():
+    return 1
